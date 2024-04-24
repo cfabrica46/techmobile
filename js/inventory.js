2307af3ca9049920b6393176f4ff70da926a5839
César Caycho <cfabrica46@gmail.com>
2024-04-24 10:31:17 -0500

    feat: update style

--- js/inventory.js
+++ js/inventory.js
@@ -1,5 +1,33 @@
-let slideIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    // Cargar la información del archivo JSON
+    fetch('../data/database.json')
+        .then(response => response.json())
+        .then(data => {
+            // Obtener el elemento del carrusel
+            const carousel = document.querySelector('.carousel');
+            
+            // Crear slides para cada marca y mostrar los logos
+            data.inventory.forEach(brand => {
+                const slide = document.createElement('div');
+                slide.classList.add('slide');
+                const img = document.createElement('img');
+                img.src = brand.logo_url;
+                img.alt = brand.brand;
+                img.classList.add('carousel-image');
+                slide.appendChild(img);
+                carousel.appendChild(slide);
+            });
+            
+            // Inicializar el carrusel
+            changeSlide(0);
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
 
+});
+
+
+// Función para cambiar el slide del carrusel
+let slideIndex = 0;
 function changeSlide(direction) {
     const slides = document.querySelectorAll('.slide');
     slides[slideIndex].classList.remove('active');
@@ -8,7 +36,3 @@ function changeSlide(direction) {
     if (slideIndex < 0) slideIndex = slides.length - 1;
     slides[slideIndex].classList.add('active');
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    changeSlide(0);
-});
